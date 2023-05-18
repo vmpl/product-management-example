4983aa1b9fcf7ac23af1d1fbf7a86c1758dfeaef
Kamil Wiśniowski <kamil.wisniowski@vm.pl>
2023-05-18 03:24:56 +0200

    Adding quasar to include component with list items

--- resources/js/Pages/Crud/List.vue
+++ resources/js/Pages/Crud/List.vue
@@ -6,10 +6,16 @@
             </h2>
         </template>
 
-        <div>
-            <ul>
-                <li v-for="grid in grids">{{ grid }}</li>
-            </ul>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
+                    <q-list bordered separator>
+                        <q-item clickable v-ripple>
+                            <q-item-section v-for="grid in grids" class="text-white">{{ grid }}</q-item-section>
+                        </q-item>
+                    </q-list>
+                </div>
+            </div>
         </div>
     </AppLayout>
 </template>
