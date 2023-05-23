f17d58d3dd2de21a858f8e5579fa4c60029261c5
Kamil Wiśniowski <kamil.wisniowski@vm.pl>
2023-05-23 23:47:41 +0200

    Change dev server

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,14 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import { quasar } from '@quasar/vite-plugin'
 import i18n from 'laravel-vue-i18n/vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
+    server: {
+        host: 'localhost',
+    },
     plugins: [
+        basicSsl(),
         laravel({
             input: 'resources/js/app.js',
             ssr: 'resources/js/ssr.js',
